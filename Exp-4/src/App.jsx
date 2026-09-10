3eb535162e3f920f8ff95a4967faac6994fc7a86
Jashanpreet-1014 <syaljashan@gmail.com>
2026-09-10 13:57:21 +0530

    Add files via upload

---
+++ Exp-4/src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./public/Home";
+import About from "./public/About";
+import Contact from "./public/Contact";
+import Dashboard from "./dashboard/Dashboard";
+import Profile from "./dashboard/Profile";
+import Settings from "./dashboard/Settings";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/dashboard">Dashboard</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="profile" element={<Profile />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
